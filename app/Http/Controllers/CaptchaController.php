8cc32b96830f0438e77c092c3c46c666bf87acf8
Minho <longfei6671@163.com>
2017-01-12 10:54:58 +0800

    更换验证码库

--- app/Http/Controllers/CaptchaController.php
+++ app/Http/Controllers/CaptchaController.php
@@ -8,7 +8,8 @@
 
 namespace SmartWiki\Http\Controllers;
 
-use Gregwar\Captcha\CaptchaBuilder;
+//use Gregwar\Captcha\CaptchaBuilder;
+use Minho\Captcha\CaptchaBuilder;
 use Storage;
 use Session;
 
@@ -20,24 +21,42 @@ class CaptchaController extends Controller
      */
     public function verify()
     {
-        header('Content-type: image/jpeg');
-        $fonts = Storage::allFiles('fonts');
-
         $builder = new CaptchaBuilder();
-        $builder->setInterpolation(0);
-        $font = storage_path('app/' . $fonts[mt_rand(0,count($fonts)-1)]);
 
-        $builder->build(110,34,$font);
+        $builder->initialize([
+            'width' => 110,     // 宽度
+            'height' => 34,     // 高度
+            'line' => false,     // 直线
+            'curve' => true,   // 曲线
+            'noise' => false,   // 噪点背景
+            'fonts' => []       // 字体
+        ]);
 
-        //获取验证码的内容
-        $phrase = $builder->getPhrase();
 
-       // 把内容存入session
+
+        $builder->create()->output(1);
+
+        $phrase = $builder->getText();
         Session::flash('milkcaptcha', $phrase);
-        $builder->output();
 
-
-        header('Content-type: image/jpeg');
-        return;
+//        header('Content-type: image/jpeg');
+//        $fonts = Storage::allFiles('fonts');
+//
+//        $builder = new CaptchaBuilder();
+//        $builder->setInterpolation(0);
+//        $font = storage_path('app/' . $fonts[mt_rand(0,count($fonts)-1)]);
+//
+//        $builder->build(110,34,$font);
+//
+//        //获取验证码的内容
+//        $phrase = $builder->getPhrase();
+//
+//       // 把内容存入session
+//        Session::flash('milkcaptcha', $phrase);
+//        $builder->output();
+//
+//
+//        header('Content-type: image/jpeg');
+//        return;
     }
 }
